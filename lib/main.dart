464ad5a9017b7019ccff0fb346be56fe0e0e9593
bhavyarabhatt <bhavyarbhatt@gmail.com>
2024-07-31 00:36:38 +0530

    API Call

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,14 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_database/models/albun.dart';
+import 'package:hive_database/pages/home_page.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:http/http.dart' as http;  
 
 void main() async{
-  WidgetsFlutterBinding.ensureInitialized();
-  final documentsDir = await getApplicationCacheDirectory();
-  Hive.init(documentsDir.path);
+  // WidgetsFlutterBinding.ensureInitialized();
+  // final documentsDir = await getApplicationCacheDirectory();
+  // Hive.init(documentsDir.path);
   runApp(const MyApp());
 }
 
@@ -23,58 +24,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(),
+      home:  HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-late Future<Album> futureAlbum;
-
-@override
-  void initState() {
-    super.initState();
-    futureAlbum = fetchAlbum();
-  }
-
-
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: FutureBuilder<Album>(
-        future: futureAlbum,
-        builder: (context, snapshot) {
-          if(snapshot.hasData) {
-              return Text(snapshot.data!.title);
-          } else if(snapshot.hasError){
-              return Text('${snapshot.error}');
-          }
-
-          return const CircularProgressIndicator();
-        },
-      ),
-    );
-  }
-  
-  Future<Album> fetchAlbum() async {
-        final response = await http.get(
-          Uri.parse('https://jsonplaceholder.typicode.com/albums/1')
-        );
-
-        if(response.statusCode == 200 ){
-          return Album.fromJson(jsonDecode(response.body)  as Map<String, dynamic>);
-        } else {
-          throw Exception('Failed to load album');
-        }
-  }
-
-}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:hive_database/models/albun.dart';
 import 'package:http/http.dart' as http;
 
 
@@ -10,12 +13,54 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  late Future<Album> futureAlbum;
+
+
+@override
+  void initState() {
+    super.initState();
+        futureAlbum = fetchAlbum();
+
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
       appBar: AppBar(
-        title: Text("Hive Database"),
-      )
+        title: Center(
+          child: Text("Hive Database")),
+      ),
+      body: Center(
+        child: FutureBuilder<Album>(
+          future: futureAlbum,
+          builder: (context, snapshot){
+            if(snapshot.hasData){
+              return Text(snapshot.data!.title);
+            }else if(snapshot.hasError){
+                return Text('${snapshot.error}');
+            }
+
+            return CircularProgressIndicator();
+          },
+        ),
+      ),
+
     );
   }
-}
+
+  Future<Album> fetchAlbum() async{
+    final response = await http.get(Uri.parse("https://jsonplaceholder.typicode.com/albums/1"));
+      if(response.statusCode == 200  ){
+    return Album.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
+      }else {
+        throw Exception("Failed to load ");
+      }
+  }
+
+
+
+  
+  }
+
